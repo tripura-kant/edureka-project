643c6b0a97a0a9d25ab6a4799524a8120481626a
Tripura kant <tripurakant@gmail.com>
2023-08-28 00:21:04 +0530

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -3,21 +3,28 @@
 # Update package repository
 yum update -y
 
-# Install Java 17 if not already installed
-if ! command -v java || ! java -version 2>&1 | grep "17.0.8" >/dev/null; then
-    echo "Java 17 not found. Installing Java 17..."
-    yum install -y java-17-openjdk-devel
-else
-    echo "Java 17 is already installed."
-fi
+# Function to install a package if not already installed
+install_package() {
+    package_name=$1
+    if ! yum list installed "$package_name" >/dev/null 2>&1; then
+        echo "$package_name not found. Installing $package_name..."
+        yum install -y "$package_name"
+    else
+        echo "$package_name is already installed."
+    fi
+}
+
+# Install Java 17
+install_package java-17-openjdk-devel
 
 # Install necessary tools
-yum install -y git
+install_package git
+install_package wget
 
 # Install Maven if not already installed
 if ! command -v mvn; then
-    echo "Maven not found. Installing Maven..."
-    yum install -y wget
+    install_package java-1.8.0-openjdk-devel # Ensure Java 8 for Maven
+    echo "Installing Maven..."
     wget https://apache.osuosl.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
     tar -xzvf apache-maven-3.8.4-bin.tar.gz -C /opt
     ln -s /opt/apache-maven-3.8.4 /opt/maven
@@ -31,7 +38,7 @@ fi
 
 # Install Jenkins if not already installed
 if ! command -v jenkins; then
-    echo "Jenkins not found. Installing Jenkins..."
+    echo "Installing Jenkins..."
     wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
     rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
     yum install -y jenkins
@@ -40,6 +47,7 @@ if ! command -v jenkins; then
 else
     echo "Jenkins is already installed."
 fi
+
 
 # Install Docker if not already installed
 if ! command -v docker; then
