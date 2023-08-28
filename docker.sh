c5fc2c884fdb724dc63b5f9e11f9193eee9d489d
Tripura kant <tripurakant@gmail.com>
2023-08-29 00:15:47 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,10 +6,10 @@ docker rm $(docker ps -aq)
 VERSION=$(awk '{ print $1+1 }' version.txt)
 
 # Build Docker Image
-sudo docker build -t your-registry/edureka-cicd:${VERSION} .
+docker build -t your-registry/edureka-cicd:${VERSION} .
 
 # Deploy Docker container with the unique tag
-sudo docker run -d -p 8081:8081 your-registry/edureka-cicd:${VERSION}
+docker run -d -p 8081:8081 your-registry/edureka-cicd:${VERSION}
 
 # Save the incremented version back to the file
 echo ${VERSION} > version.txt
